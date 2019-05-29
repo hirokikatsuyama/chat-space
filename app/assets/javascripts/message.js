2de456c337d4cc5b4b24fdc424153f5eafcff7f9
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-05-29 11:53:31 +0900

    inputタグを訂正　　クラス名が違ってた

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,42 +20,52 @@ $(function() {
                 </div>`
   return html;
   }
-
+  
   
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
-    console.log("message");
-    var message = new FormData(this)
-    var url = $(this).attr('action')   
+   
+    
+    var message = new FormData(this);   
+    var url = $(this).attr('action');  
     $.ajax({
       url: url,
       type: "POST",
-      data: formData,
+      data: message,
       dataType: 'json',
       processData: false,
       contentType: false
     })
 
-
+    
     .done(function(data){
+      
       var html = buildHTML(data);
       $('.messages').append(html)
       $('.message_content').val('')
     })
+    
     .fail(function(){
       alert('error');
     })
-    always(function(data){
-      $('.submit-btn').prop('disabled', false);　//ここで解除している
-    })
 
-    function scrollBottom(){
+   
+
+    .always(function(data){
+    
+      $('.form__submit').attr('disabled', false);　
+    })
+    
+
+   
+    function scrollBottom(){        
       var target = $('.message').last();
       var position = target.offset().top + $('.messages').scrollTop();
       $('.messages').animate({
         scrollTop: position
       }, 300, 'swing');
     }
+    
 
 
   });
