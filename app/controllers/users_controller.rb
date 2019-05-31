e56899fd54dc3ddf3a102e797dbc06c10c1b23cc
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-05-31 15:07:12 +0900

    構文をシンプルにした　attr削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
+    if update(user_params)
       redirect_to root_path
     else
       render :edit
