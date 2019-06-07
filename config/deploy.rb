9853b66c123d3922990ccfdc77e1fa0e94464bef
hirokikatsuyama <50540371+hirokikatsuyama@users.noreply.github.com>
2019-06-07 20:19:04 +0900

    再修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,6 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-lock '<Capistranoのバージョン>'
 
 set :application,'chat-space'
 set :repo_url,  'git@github.com:hirokikatsuyama/chat-space.git'
